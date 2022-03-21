edc53716aee04c96e7282c525b40dcbf859ff3b7
Acoleman37 <aleccoleman123@yahoo.com>
2022-03-20 18:03:26 -0700

    .

--- server/server.js
+++ server/server.js
@@ -1,42 +1,47 @@
-const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
+const express = require('express');
+const path = require('path');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
-// importing apollo server
-const { ApolloServer } = require("apollo-server-express");
+//db connection
+const db = require('./config/connection');
 
-// importing authMiddleware
-const { authMiddleware } = require("./utils/auth");
+// const routes = require('./routes');
 
-// requiring typedefs
-const { typeDefs, resolvers } = require("./schemas");
-
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// importing the servers middle ware
+//apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context: authMiddleware
 });
 
+//apply apollo server with express app
 server.applyMiddleware({ app });
 
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// app.use(routes);
+
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-db.once("open", () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
