34e44e8bdd4159319169c83f4203e6a500bd28fd
Acoleman37 <aleccoleman123@yahoo.com>
2022-03-20 18:21:42 -0700

    server bug

--- server/server.js
+++ server/server.js
@@ -1,42 +1,35 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
-//import apollo server
-const { ApolloServer } = require('apollo-server-express');
-// import typeDefs and resolvers
-const { typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
 
-//db connection
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
-// const routes = require('./routes');
-
-//express server
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-//apollo server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
-//apply apollo server with express app
-server.applyMiddleware({ app });
+startServer()
 
-//middleware parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
-//get all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
@@ -44,6 +37,5 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
